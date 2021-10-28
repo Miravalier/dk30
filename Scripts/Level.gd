f058198d0602cb0f2bc05466602dda48b0f5010e
Brett Beekley <brettbeekley@gmail.com>
2021-10-27 21:01:56 -0700

    Player can enter an expression for wind

--- Scripts/Level.gd
+++ Scripts/Level.gd
@@ -7,7 +7,9 @@ var orb_scene = null
 var spawn = null
 var time = 0
 var gravity_formula = Expression.new()
-var wind_formula = Expression.new()
+var wind = 0
+var gravity = 0
+
 
 func _ready():
 	# Global map that stores formula values
@@ -23,29 +25,39 @@ func _ready():
 	make_orb()
 	# Debug formulas
 	gravity_formula.parse("1 - time", variables.keys())
-	wind_formula.parse("1", variables.keys())
+
 
 func _physics_process(delta):
-	# Keep track of time elapsed as a formula variable
+	# Keep track of time elapsed as a formula variable.
 	time += delta
 	variables.set("time", time)
-	# If there is no orb, don't worry about wind or gravity
+	# If there is no orb, don't worry about wind or gravity.
 	if orb == null:
 		return
-	# Evaluate wind and gravity formulas
-	var wind = wind_formula.execute(variables.values())
-	var gravity = gravity_formula.execute(variables.values())
-	# Check for success
-	if wind_formula.has_execute_failed():
-		print("Wind expression execution failed: %s", wind_formula.get_error_text())
-		return
-	if gravity_formula.has_execute_failed():
-		print("Gravity expression execution failed: %s", gravity_formula.get_error_text())
-		return
-	# Apply physics to the orb
-	orb.add_central_force(Vector2(0, gravity))
+	# Read wind and gravity forces.
+	# Apply physics to the orb.
+	orb.add_central_force(Vector2(wind, gravity))
+
 
 func make_orb():
 	orb = orb_scene.instance()
 	orb.position = spawn.position
 	scene.add_child(orb)
+
+
+func take_stroke():
+	# Disable button.
+	var take_stroke_button = find_node("TakeStrokeButton")
+	take_stroke_button.disabled = true
+	# Parse expression from inputs.
+	var wind_string = "0" if find_node("WindInput").text == "" else find_node("WindInput").text
+	var wind_formula = Expression.new()
+	wind_formula.parse(wind_string, variables.keys())
+	print(wind_string)
+	# Execute expression and assaign to global var.
+	wind = wind_formula.execute(variables.values())
+	# If it fails, return error.
+	if wind_formula.has_execute_failed():
+		print("Wind expression execution failed: %s", wind_formula.get_error_text())
+		take_stroke_button.disabled = false
+		return
