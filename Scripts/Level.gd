6db13ab8997a611641d5e9464662911483df4962
Brett Beekley <brettbeekley@gmail.com>
2021-10-27 21:41:59 -0700

    Add gravity

--- Scripts/Level.gd
+++ Scripts/Level.gd
@@ -7,6 +7,7 @@ var orb_scene = null
 var spawn: Node2D = null
 var time: float = 0
 var wind_formula: Expression = null
+var gravity_formula: Expression = null
 
 
 func _ready():
@@ -32,8 +33,9 @@ func _physics_process(delta: float):
 		return
 	# Read wind and gravity forces.
 	var wind: float = 0 if wind_formula == null else wind_formula.execute(variables.values())
+	var gravity: float = 0 if gravity_formula == null else gravity_formula.execute(variables.values())
 	# Apply physics to the orb.
-	orb.add_central_force(Vector2(wind, 0))
+	orb.add_central_force(Vector2(wind, gravity))
 
 
 func make_orb():
@@ -43,18 +45,33 @@ func make_orb():
 
 
 func take_stroke():
+	time = 0
 	# Disable button.
 	var take_stroke_button: Button = find_node("TakeStrokeButton")
 	take_stroke_button.disabled = true
+	
 	# Parse expression from inputs.
-	var wind_input: String = "0" if find_node("WindInput").text == "" else find_node("WindInput").text
+	var wind_input: LineEdit = find_node("WindInput")
+	var wind_input_val: String = "0" if wind_input.text == "" else wind_input.text
 	# Reset the expression.
-	time = 0
 	wind_formula = Expression.new()
-	wind_formula.parse(wind_input, variables.keys())
+	wind_formula.parse(wind_input_val, variables.keys())
 	wind_formula.execute(variables.values())
 	# Test execute expression. If it fails, return error.
 	if wind_formula.has_execute_failed():
 		print("Wind expression execution failed: ", wind_formula.get_error_text())
+		take_stroke_button.disabled = false
+		return
+		
+	# Parse expression from inputs.
+	var gravity_input: LineEdit = find_node("WindInput")
+	var gravity_input_val: String = "0" if gravity_input.text == "" else gravity_input.text
+	# Reset the expression.
+	gravity_formula = Expression.new()
+	gravity_formula.parse(gravity_input_val, variables.keys())
+	gravity_formula.execute(variables.values())
+	# Test execute expression. If it fails, return error.
+	if gravity_formula.has_execute_failed():
+		print("Gravity expression execution failed: ", gravity_formula.get_error_text())
 		take_stroke_button.disabled = false
 		return
